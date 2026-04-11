a57f8cb973bdb72035859ebd851e4aa1ba31d821
Ben Gardiner <ben.l.gardiner@gmail.com>
2026-04-11 12:13:37 -0400

    make sure to deploy the resource json_files

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         ],
     },
     packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
     entry_points={
         'console_scripts': [
             'truckdevil = truckdevil.truckdevil:main',
